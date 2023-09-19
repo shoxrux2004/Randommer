cc1fa87519ae881858674540b7c0867b3482c3a1
shoxrux <shoxruxnorqoziyev2004@gmail.com>
2023-09-19 14:53:33 +0500

    name info

--- name.py
+++ name.py
@@ -14,7 +14,12 @@ class Name(Randommer):
         Returns:
             list: list of names
         '''
-        pass
+        url=f"{self.get_url()}Name?nameType=firstname&quantity=1"
+        headers={
+            "X-Api-Key":api_key
+        }
+        response=requests.get(url,headers=headers)
+        return response.json()
     
     def get_name_suggestions(self, api_key: str, startingWords: str) -> list:
         '''get name suggestions
@@ -26,7 +31,12 @@ class Name(Randommer):
         Returns:
             list: list of name suggestions
         '''
-        pass
+        url=f"{self.get_url()}Name/Suggestions?startingWords=a"
+        headers={
+            "X-Api-Key":api_key
+        }
+        response=requests.get(url,headers=headers)
+        return response.json()
     
     def get_name_cultures(self, api_key: str) -> list:
         '''get available cultures
@@ -37,4 +47,12 @@ class Name(Randommer):
         Returns:
             list: list of names
         '''
-        pass
+        url=f"{self.get_url()}Name/Cultures"
+        headers={
+            "X-Api-Key":api_key
+        }
+        response=requests.get(url,headers=headers)
+        return response.json()
+
+o=Name()
+print(o.get_name_cultures("64fd8245b648443a9aea0fbd8ce5a9ae"))
