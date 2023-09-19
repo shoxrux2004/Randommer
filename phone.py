f48f618c1c4b77ec6f57cc9b68c287db70fc88fb
shoxrux <shoxruxnorqoziyev2004@gmail.com>
2023-09-19 14:47:19 +0500

    phone info

--- phone.py
+++ phone.py
@@ -1,6 +1,6 @@
 import requests
 from randommer import Randommer
-
+from pprint import pprint
 
 class Phone(Randommer):
     def generate(self, api_key: str, CountryCode: str, Quantity: int) -> list:
@@ -14,7 +14,12 @@ class Phone(Randommer):
         Returns:
             list: list of phone numbers
         '''
-        pass
+        url=f"{self.get_url()}Phone/Generate?CountryCode=uz&Quantity=1"
+        headers={
+            "X-Api-Key":api_key
+        }
+        response=requests.get(url,headers=headers)
+        return response.json()
     
     def get_IMEI(self, api_key: str, Quantity: int) -> list:
         '''get bulk imei
@@ -26,7 +31,12 @@ class Phone(Randommer):
         Returns:
             list: list of phone numbers
         '''
-        pass
+        url=f"{self.get_url()}Phone/IMEI?Quantity=1"
+        headers={
+            "X-Api-Key":api_key
+        }
+        response=requests.get(url,headers=headers)
+        return response.json()
     
     def is_valid(self, api_key: str, telephone: str, CountryCode: str) -> bool:
         '''get bulk imei
@@ -39,7 +49,12 @@ class Phone(Randommer):
         Returns:
             bool: is valid
         '''
-        pass
+        url=f"{self.get_url()}Phone/Validate?telephone=931166974&CountryCode=uz"
+        headers={
+            "X-Api-Key":api_key
+        }
+        response=requests.get(url,headers=headers)
+        return response.json()
     
     def get_countries(self, api_key: str) -> list:
         '''get countries
@@ -50,4 +65,13 @@ class Phone(Randommer):
         Returns:
             list: lsit of countries
         '''
-        pass
+        url=f"{self.get_url()}Phone/Countries"
+        headers={
+            "X-Api-Key":api_key
+        }
+        response=requests.get(url,headers=headers)
+        return response.json()
+    
+
+o=Phone()
+pprint(o.get_countries("64fd8245b648443a9aea0fbd8ce5a9ae"))
